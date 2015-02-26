b95d0d1a28d313ad58f53d19ea477593c3429018
azlux <azlux@outlook.com>
2015-02-27 00:10:59 +0200

    Update notes.php

--- pages/notes.php
+++ pages/notes.php
@@ -6,13 +6,13 @@
                     <textarea tabindex="1" name="note" id="note" placeholder="Ajouter une note..." required></textarea>
                     <input type="submit" tabindex="3" id="valider" value="OK" />
                     <div class="clear"></div>
-		</form>';
+		</form><div id="container_notes">';
 	
         $query = $sqlite->query('SELECT * FROM notes WHERE user_id=' . $_SESSION['id'] . ' ORDER BY date DESC');
         while($result = $query->fetch())
             echo '<div class="note rouge" id="n' . $result['id'] . '"><h1>' . $result['title'] . '</h1><a class="supprimer" href="core/interact.php?action=supprimer_note&token=' . $_SESSION['token'] . '&id=' . $result['id'] . '">X</a><p>' . $result['note'] . '</p></div>';
         
-        echo '</div>';
+        echo '</div></div>';
         
         $title = 'Mes notes';
     }
