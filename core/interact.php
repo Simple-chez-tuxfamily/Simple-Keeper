d1655539708630b97025798ac338a9cee931d39e
azlux <azlux@outlook.com>
2015-05-12 19:54:06 +0300

    $_GET['token'] error
    tu ne peut pas modifier $_GET['token']  car c'est une info venant du client en lecture seule. Le serveur renvoie une erreur à chaque fois.

--- core/interact.php
+++ core/interact.php
@@ -4,21 +4,28 @@
      * Usage : interact.php?action=fichier[&parametre1][&parametre2]
      * Attention: fichier ne doit PAS être accompagné de l'extension PHP
     */
-    
+
     header('Content-Type: text/html; charset=utf-8'); // Encodage
-        
+
     session_start();
-        
-    if(!isset($_GET['token']))
-        $_GET['token'] = '';
-        
-    if($_GET['token'] == $_SESSION['token'])
+
+
+
+$token;
+    if (!isset($_GET['token'])) {
+        $token = '';
+    } else {
+        $token = $_GET['token'];
+    }
+
+    if($token == $_SESSION['token'])
         $has_access = true;
+
     elseif($_GET['action'] == 'connect')
         $has_access = true;
     else
         $has_access = false;
-        
+
     if(!empty($_GET['action']) && file_exists('interact/' . $_GET['action'] . '.php') && $has_access == true)
         include './interact/' . $_GET['action'] . '.php';
     else
